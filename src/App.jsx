e804a7c6a04b846b856dac1971ec805e1e323219
Norman <norman@advancedgroup.com>
2024-08-04 23:58:54 +0700

    Create Website Restoran

---
+++ src/App.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+
+import data from "./utils/data";
+import "./styles/index.css";
+import "./App.css";
+
+function Header() {
+  return <h1 className="menu">Restoran Mang Guntur</h1>;
+}
+
+function Menu() {
+  const foods = data;
+  // const foods = [];
+  const numFoods = foods.length;
+
+  if (numFoods === 0) {
+    return (
+      <main className="menu">
+        <p>Menu Kosong, Besok datang lagi</p>
+      </main>
+    );
+  } else {
+    return (
+      <menu className="menu">
+        <h2>Menu Kita:</h2>
+        <p>
+          Aneka makanan Indonesia yang disajikan oleh warteg mang udin sebagai
+          pemenuhan makanan kesehatan yang diperlukan dalam kehidupan
+          sehari-hari.
+        </p>
+        <h3>Makanan</h3>
+        <ul className="foods">
+          {data.map((food) => (
+            <Food foodObj={food} key={food.name} />
+          ))}
+        </ul>
+      </menu>
+    );
+  }
+}
+
+function Food(props) {
+  const { name, description, price, image, stock } = props.foodObj;
+  return (
+    <li className={`food ${!stock ? "sold-out" : ""}`}>
+      <img src={image} alt={name} width={150} />
+      <div>
+        <h3>{name}</h3>
+        <p>{description}</p>
+        <span>Rp. {price}</span>
+        <article>{stock ? "Tersedia" : "Stok habis"}</article>
+      </div>
+    </li>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openClock = 10;
+  const closeClock = 23;
+  const isOpen = hour >= openClock && hour <= closeClock;
+
+  if (isOpen) {
+    return <FooterOpenHour openClock={openClock} closeClock={closeClock} />;
+  } else {
+    return <FooterCloseHour openClock={openClock} closeClock={closeClock} />;
+  }
+}
+
+function FooterOpenHour({ openClock, closeClock }) {
+  return (
+    <footer className="footer">
+      <div className="order">
+        <p>
+          Restoran Mang Guntur buka pukul:
+          {openClock} - {closeClock}
+        </p>
+        <button className="btn">Order</button>
+        <small>Klik tombol diatas untuk memesan</small>
+      </div>
+    </footer>
+  );
+}
+
+function FooterCloseHour({ openClock, closeClock }) {
+  return (
+    <footer className="footer">
+      <p>
+        Maaf Kak Masih tutup, Coba dateng lagi pukul: {openClock} - {closeClock}
+      </p>
+    </footer>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Header />
+        <hr />
+        <Menu />
+        <hr />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/utils/data.js
@@ -0,0 +1,96 @@
+const foodMenu = [
+  {
+    name: "Nasi Goreng",
+    description: "Nasi goreng dengan bumbu kacang rempah khas indonesia",
+    price: 19000,
+    image:
+      "https://images.unsplash.com/photo-1680674774705-90b4904b3a7f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8TmFzaSUyMGdvcmVuZ3xlbnwwfHwwfHx8MA%3D%3D",
+    stock: Math.random() >= 0.5 ? true : false,
+  },
+  {
+    name: "Nasi Kuning",
+    description: "Nasi Kuning Lezat Buatan Mak Cihuy khas indonesia",
+    price: 13000,
+    image:
+      "https://media.istockphoto.com/id/2088502589/id/foto/nasi-kuning-or-yellow-rice.jpg?s=612x612&w=is&k=20&c=JQjjeGorIQnYu3Tjb-B47t3iCv-UZZ4OqLYFLDeqfc0=",
+    stock: Math.random() >= 0.5 ? true : false,
+  },
+  {
+    name: "Spageti Dengan Sauce Tomat & Daging",
+    description: "Spageti dengan bumbu Tomat dan daging rempah khas Malaysia",
+    price: 25000,
+    image:
+      "https://media.istockphoto.com/id/637214478/photo/pasta-plate.webp?b=1&s=170667a&w=0&k=20&c=5zJT5eah3c_O4gj4pb2hDyVpViWthUAjmkiI-ZbjTxc=",
+    stock: Math.random() >= 0.5 ? true : false,
+  },
+  {
+    name: "Sate Ayam",
+    description:
+      "Potongan daging ayam yang ditusuk dan dibakar, disajikan dengan bumbu kacang",
+    price: 15000,
+    image: "/src/assets/food/sate-ayam.jpg",
+    stock: Math.random() >= 0.5 ? true : false,
+  },
+  {
+    name: "Gado-Gado",
+    description: "Sayuran segar dengan bumbu kacang khas Indonesia",
+    price: 20000,
+    image: "/src/assets/food/gado-gado.jpg",
+    stock: Math.random() >= 0.5 ? true : false,
+  },
+  {
+    name: "Rendang",
+    description:
+      "Daging yang dimasak dengan santan dan rempah-rempah khas Padang",
+    price: 30000,
+    image: "/src/assets/food/rendang.jpg",
+    stock: Math.random() >= 0.5 ? true : false,
+  },
+  {
+    name: "Soto Ayam",
+    description: "Sup ayam dengan kuah kuning dan berbagai rempah",
+    price: 18000,
+    image: "/src/assets/food/soto-ayam.jpg",
+    stock: Math.random() >= 0.5 ? true : false,
+  },
+  {
+    name: "Martabak",
+    description:
+      "Makanan dadakan yang terbuat dari adonan tepung dengan berbagai isian",
+    price: 20000,
+    image: "/src/assets/food/martabak.jpg",
+    stock: Math.random() >= 0.5 ? true : false,
+  },
+  {
+    name: "Bakso",
+    description: "Bola daging yang direbus disajikan dengan mie dan kuah kaldu",
+    price: 12000,
+    image: "/src/assets/food/bakso.jpg",
+    stock: Math.random() >= 0.5 ? true : false,
+  },
+  {
+    name: "Soto Betawi",
+    description: "Sup daging dengan kuah santan kental khas Betawi",
+    price: 22000,
+    image: "/src/assets/food/soto-betawi.jpg",
+    stock: Math.random() >= 0.5 ? true : false,
+  },
+  {
+    name: "Ayam Penyet",
+    description:
+      "Ayam yang digeprek dengan bumbu rempah dan disajikan dengan sambal",
+    price: 18000,
+    image: "/src/assets/food/ayam-penyet.jpg",
+    stock: Math.random() >= 0.5 ? true : false,
+  },
+  {
+    name: "Ayam Penyet",
+    description:
+      "Ayam yang digeprek dengan bumbu rempah dan disajikan dengan sambal",
+    price: 18000,
+    image: "/src/assets/food/ayam-penyet.jpg",
+    stock: Math.random() >= 0.5 ? true : false,
+  },
+];
+
+export default foodMenu;
